47e7caaf07d2934091be334ecbb6ce8c4d9ae6e0
gitsayed <abucollege@gmail.com>
2023-07-27 20:12:17 +0600

    commit day 27 / 07 /2023

--- frontend/src/app/_services/user.service.ts
+++ frontend/src/app/_services/user.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { baseURL } from '../constants';
 
-const API_URL =  environment.baseURL+'/api/v1/test/';
+const API_URL =  baseURL+'/api/v1/test/';
 
 @Injectable({
   providedIn: 'root'
